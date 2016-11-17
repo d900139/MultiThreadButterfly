43c4fd84f21093d324f5b59b80d50e718ee6601c
KevinChang <d900139@yahoo.com.tw>
2016-11-17 21:01:41 +0800

    Update project

--- app/src/main/java/yzuic/kevinchang/multithreadbutterfly/MainActivity.java
+++ app/src/main/java/yzuic/kevinchang/multithreadbutterfly/MainActivity.java
@@ -2,12 +2,34 @@ package yzuic.kevinchang.multithreadbutterfly;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
+    private LinearLayout ll;
+    private Button btnGo;
+    private ImageView iv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ll = (LinearLayout) findViewById(R.id.mainLayout);
+        btnGo = new Button(this);
+        iv = new ImageView(this);
+
+        btnGo.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
+
+        ll.addView(iv);
+        ll.addView(btnGo);
     }
 }
