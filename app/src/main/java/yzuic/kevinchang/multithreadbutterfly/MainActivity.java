79c02b4456662919efadb3362e125e0c5b56dfda
KevinChang <d900139@yahoo.com.tw>
2016-11-23 22:41:27 +0800

    Update project, add new thread to let butterfly flying.

--- app/src/main/java/yzuic/kevinchang/multithreadbutterfly/MainActivity.java
+++ app/src/main/java/yzuic/kevinchang/multithreadbutterfly/MainActivity.java
@@ -1,5 +1,7 @@
 package yzuic.kevinchang.multithreadbutterfly;
 
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,11 +9,39 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private LinearLayout ll;
-    private Button btnGo;
     private ImageView iv;
+    private Button btnGo;
+    private TextView tv;
+    int img = 0;
+    int[] pic = {R.drawable.up, R.drawable.mid, R.drawable.down};
+    boolean flag = false;
+
+    Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+        }
+    };
+
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            iv.setBackgroundResource(pic[img]);
+            img = (img + 1) % 3;
+            handler.postDelayed(this, 200);
+        }
+    };
+
+    @Override
+    protected void onDestroy() {
+        if (handler != null)
+            handler.removeCallbacks(runnable);
+        super.onDestroy();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,17 +49,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ll = (LinearLayout) findViewById(R.id.mainLayout);
-        btnGo = new Button(this);
         iv = new ImageView(this);
-
-        btnGo.setOnClickListener(new Button.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                
-            }
-        });
+        btnGo = new Button(this);
+        tv = new TextView(this);
+        tv.setTextSize(20);
 
         ll.addView(iv);
         ll.addView(btnGo);
+        ll.addView(tv);
+
+        Thread tr = new Thread(runnable);
+        tr.start();
+
+        btnGo.setText("Go");
+        btnGo.setTextSize(20);
+        btnGo.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                flag = !flag;
+                if (flag) {
+                    tv.setText("Kevin Chang");
+                } else {
+                    tv.setText("");
+                }
+            }
+        });
     }
 }
